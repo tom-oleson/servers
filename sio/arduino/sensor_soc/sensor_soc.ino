c792b573a8ca43f317da688087887603245a3080
Tom Oleson <tom.oleson@gmail.com>
2019-11-24 09:37:09 +0000

    Remove + prefix for hostname (name in router)

--- sio/arduino/sensor_soc/sensor_soc.ino
+++ sio/arduino/sensor_soc/sensor_soc.ino
@@ -372,7 +372,7 @@ bool init_wifi() {
             mac[3], mac[4], mac[5]);
 
     snprintf(hostname, sizeof(hostname),
-           "+" DEVICE_PREFIX "-%02X%02X%02X%02X%02X%02X",
+           DEVICE_PREFIX "-%02X%02X%02X%02X%02X%02X",
             mac[0], mac[1], mac[2],
             mac[3], mac[4], mac[5]);
 
